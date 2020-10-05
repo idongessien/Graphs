ed970720548a351b3cb2846734e17f3bf8aa33ba
Idong Essien <idong.essien@comcast.net>
2020-10-05 18:42:08 -0400

    implemented graph class with specified labels and edges

--- projects/graph/graph.py
+++ projects/graph/graph.py
@@ -7,7 +7,12 @@ class Graph:
 
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
     def __init__(self):
-        self.vertices = {}
+        self.vertices = {
+            '0': {'1', '3'},
+            '1': {'0'},
+            '2': set(),
+            '3': {'0'}
+        }
 
     def add_vertex(self, vertex_id):
         """
